bde05cfb9176646117f99e5f8f574c1337bc94e0
Jeremy Ottley <Gazaunga@gmail.com>
2019-08-19 07:31:21 -0400

    Update transformator.rb

--- lib/ptnlint/transformator.rb
+++ lib/ptnlint/transformator.rb
@@ -1,7 +1,10 @@
 require 'parslet'
 
-module PTNLint
+module PGNLint
   class Transform < Parslet::Transform
-    
+    rule(:tag_name => simple(:t), :tag_value => simple(:n)) {{t.to_s => n.to_s} }
+
+    #rule(:move_number => simple(:x)) { }
+
   end
 end
